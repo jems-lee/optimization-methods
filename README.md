ab7e64afb2f0c5631af4ec4587bbff08fadad696
jems-lee <jameslee.j7@gmail.com>
2021-11-09 19:04:42 -0500

    Add details to README.md. Slightly reorganize newtons_method.py.

--- README.md
+++ README.md
@@ -2,31 +2,41 @@
 
 Convex and non-convex optimization methods from scratch.
 Key idea and implementation for each algorithm and use cases.
-
-$ min_x f(x) $
-
-subject to $g_i(x) <= 0$ for $i = 1, ..., n$
-
-$Ax = b$
+Notes are from STAT 6020 taught by Professor Tianxi Li.
 
 ## Convex Problems
-
-Key ideas:
-- Local minimum is the global minimum
 
 ### First Order Methods
 
 #### Gradient Descent
 
+- 1st order method without constraints
+
 #### Proximal Gradient Descent
 
+- 1st order methods with constraints.
+- Optimization function is the sum of two convex functions.
+  - First is complicated but differentiable.
+  - Second is simple but not necessarily differentiable (i.e. L1 norm).
+
 #### Sub-gradient Method
+
+- Applies to non-differentiable problems, with or without constraints.
+- Ideal step size is 1/t.
 
 ### Second Order Methods
 
 #### Newton's Method
 
+- 2nd order method without constraints
+- Solve a quadratic approximation to the function at each iteration.
+- Backtracking line search to find acceptable step size.
+- Locally quadratic convergence (linear while far from local optimal).
+- Can extend to problems with equality constraints.
+
 #### Log-Barrier Method
+
+- 2nd order method with constraints.
 
 #### Primal-Dual Interior Point Methods
 
@@ -34,9 +44,15 @@ Key ideas:
 
 #### Coordinate Descent
 
+- Can solve non-convex problems where the non-convex portion is additive in each component.
+- Solve each individual (or block) parameter one at a time.
+
 ## Non-convex problems
 
 ### Majorization-minimization (MM) strategy
+
+- Surrogate function lies above the optimization function and is tangent at one point.
+- The key idea is to construct a surrogate function in a smart way. 
 
 ### Difference of Convex Functions (DC)
 
--- src/newtons_method.py
+++ src/newtons_method.py
@@ -5,7 +5,7 @@ logger = logging.getLogger(__name__)
 
 
 def newtons_method(
-    beta0: np.ndarray,
+    theta0: np.ndarray,
     X: np.ndarray,
     y: np.ndarray,
     f,
@@ -13,50 +13,71 @@ def newtons_method(
     hessian,
     gap,
     eps,
-    backtracking,
     bt_factor,
+    max_iter=50,
     verbose=False,
 ):
+    """
+    Performs newton's method with backtracking line search.
+
+    Parameters
+    ----------
+    theta0
+    X
+    y
+    f
+    grad
+    hessian
+    gap
+    eps
+    bt_factor
+    max_iter
+    verbose
+
+    Returns
+    -------
+
+    """
     # start = time.perf_counter()
-    gap_k = gap(beta0, X, y)
+    gap_k = gap(theta0, X, y)
     gaps = [gap_k]
     fs = []
-    beta = beta0
+    theta = theta0
     counter = 0
-    is_converged = True
+    is_converged = False
 
     # time_per_iteration = []
-    while gap_k > eps:
+    while counter < max_iter:
+        if gap_k < eps:
+            is_converged = True
+            break
         # start_per_iter = time.perf_counter()
         counter += 1
         if verbose:
-            print(f"Newton step: {counter}")
-        f_k = f(beta, X, y)
-        grad_k = grad(beta, X, y)
-        hessian_k = hessian(beta, X, y)
+            logger.info(f"Newton step: {counter}")
+        f_k = f(theta, X, y)
+        grad_k = grad(theta, X, y)
+        hessian_k = hessian(theta, X, y)
         nu_k = -np.linalg.solve(hessian_k, grad_k)
         t = 1
-        if backtracking:
-            bt_counter = 0
-            lhs = f(beta + t * nu_k, X, y)
+        bt_counter = 0
+        lhs = f(theta + t * nu_k, X, y)
+        rhs = f_k + 0.5 * t * np.dot(grad_k, nu_k)
+        while lhs > rhs:
+            bt_counter += 1
+            t = bt_factor * t
+            lhs = f(theta + t * nu_k, X, y)
             rhs = f_k + 0.5 * t * np.dot(grad_k, nu_k)
-            while lhs > rhs:
-                bt_counter += 1
-                t = bt_factor * t
-                lhs = f(beta + t * nu_k, X, y)
-                rhs = f_k + 0.5 * t * np.dot(grad_k, nu_k)
-            if verbose:
-                print(f"Number of backtracking steps: {bt_counter}")
-            beta = beta + t * nu_k
-            gap_k = gap(beta, X, y)
-            gaps.append(gap_k)
-            fs.append(f_k)
-            if verbose:
-                print(f"Current f: {f_k}")
-                print(f"Current gap: {gap_k}")
-            if counter > 50:
-                is_converged = False
-                break
+        if verbose:
+            logging.info(f"Number of backtracking steps: {bt_counter}")
+        theta = theta + t * nu_k
+        gap_k = gap(theta, X, y)
+        gaps.append(gap_k)
+        fs.append(f_k)
+        if verbose:
+            logging.info(f"Current f: {f_k}")
+            logging.info(f"Current gap: {gap_k}")
+
         # end_per_iter = time.perf_counter()
         # time_per_iteration.append(round(end_per_iter-start_per_iter,3))
     # end = time.perf_counter()
@@ -68,4 +89,4 @@ def newtons_method(
     # else:
     # print("Convergence failed")
     # print(f"Newton steps: {counter}")
-    return fs, gaps, is_converged, beta
+    return fs, gaps, is_converged, theta
